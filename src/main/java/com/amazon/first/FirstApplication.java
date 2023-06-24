c5b64480cc87ab50a9a4c21c6f193dcab30200e2
Luis Santos <lhsantos89@gmail.com>
2023-06-24 02:21:11 -0300

    first solution

--- src/main/java/com/amazon/first/FirstApplication.java
+++ src/main/java/com/amazon/first/FirstApplication.java
@@ -3,7 +3,12 @@ package com.amazon.first;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class FirstApplication {
@@ -16,17 +21,45 @@ public class FirstApplication {
 			"man", "sunglasses", "sun", "glasses",
 			"blackboard", "black", "board","football",
 			"foot",	"ball",	"moonlight",
-			"moon",	"light","toothbrush","tooth",
+			"moon",	"toothbrush","tooth",
 			"brush","starfish",	"star",	"fish",	"wheelchair",
 			"wheel", "chair", "lighthouse", "light",
-			"house", "firefly",	"fire",	"fly", "rockstar", "rock",
+			"house", "firefly",	"fly", "rockstar", "rock",
 			"rockstars", "tar", "rocks"
 		};
 
 		Set<String> words = Set.of(entry);
+        List<List<String>> groups = new ArrayList<>();
 
-		words.stream().forEach(System.out::println);
+		words.stream().forEach(word -> {
+			StringBuffer partialWord = new StringBuffer();
 
-	}
+            List<String> group = new ArrayList<>();
+
+            List<Character> letters = Pattern.compile("")
+                                             .splitAsStream(word)
+                                             .map(i -> i.charAt(0))
+                                             .collect(Collectors.toList());
+
+            for (Character letter : letters) {
+                partialWord.append(letter);
+
+                if (words.contains(partialWord.toString())){
+                    group.add(partialWord.toString());
+                    partialWord = new StringBuffer();
+                }
+
+			}
+
+            if (group.size() > 1) {
+                groups.add(group);
+            }
+
+			System.out.println("");
+		});
+
+        System.out.println("Finito");
+
+    }
 
 }
